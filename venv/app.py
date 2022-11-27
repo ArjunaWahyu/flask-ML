42b783b6d74c7d454992abcc60e8e885564f6539
ArjunaWahyu <arjunawahyu0007@gmail.com>
2022-11-27 18:35:01 +0700

    first commit

---
+++ venv/app.py
@@ -0,0 +1,50 @@
+import os
+from pyexpat import model
+import numpy as np
+import flask
+import pickle
+from flask import Flask, redirect, url_for, request, render_template
+
+
+# creating instance of the class
+app = Flask(__name__, template_folder='templates')
+
+# to tell flask what url should trigger the function index()
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
+
+
+# prediction function
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1, 2)
+    loaded_model = pickle.load(open("./model/model.pkl", "rb"))  # load the model
+    # predict the values using loded model
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    if request.method == 'POST':
+        name = request.form['name']
+        reading_score = request.form['reading_score']
+        writing_score = request.form['writing_score']
+
+        to_predict_list = list(map(float, [reading_score, writing_score]))
+        result = ValuePredictor(to_predict_list)
+
+        if float(result) == 0:
+            prediction = 'Your exam is perfect'
+        elif float(result) == 1:
+            prediction = 'Your exam isn\'t passed'
+        elif float(result) == 2:
+            prediction = 'Your exam is passed'
+
+        return render_template("result.html", prediction=prediction, name=name)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)  # use debug = False for jupyter notebook
